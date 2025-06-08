591356a4e83d9614d113085de59e1bbc1cfdde24
SALOFOGOIUD <salomon.forero@est.iudigital.edu.co>
2025-06-08 13:08:41 -0500

    Update and rename driverUser.js to driveruser.js

--- Back-end/Models/driveruser.js
+++ Back-end/Models/driveruser.js
@@ -1,23 +1,23 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-const driverUserSchema = new mongoose.Schema({
+const driveruserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   nombre: { type: String, required: true },
   clave: { type: String, required: true }
 });
 
-driverUserSchema.pre('save', async function (next) {
+driveruserSchema.pre('save', async function (next) {
   if (!this.isModified('clave')) return next();
   const salt = await bcrypt.genSalt(10);
   this.clave = await bcrypt.hash(this.clave, salt);
   next();
 });
 
-driverUserSchema.methods.validarClave = async function (claveIngresada) {
+driveruserSchema.methods.validarClave = async function (claveIngresada) {
   return await bcrypt.compare(claveIngresada, this.clave);
 };
 
-const driverUser = mongoose.model('driverUser', driverUserSchema);
+const driveruser = mongoose.model('driveruser', driveruserSchema);
 
-module.exports = driverUser;
+module.exports = driveruser;
