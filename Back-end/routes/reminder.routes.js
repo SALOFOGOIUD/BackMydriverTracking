5172799cda68b20ec227ec6115155bac7f02ae38
SALOFOGOIUD <salomon.forero@est.iudigital.edu.co>
2025-06-08 13:25:25 -0500

    Update reminder.routes.js

--- Back-end/routes/reminder.routes.js
+++ Back-end/routes/reminder.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Reminder = require('../Models/reminder');
-const auth = require('../middlewares/auth');
+const auth = require('../Middlewares/auth');
 
 router.get('/', auth, async (req, res) => {
   const recordatorios = await Reminder.find({ idUser: req.user.id });
